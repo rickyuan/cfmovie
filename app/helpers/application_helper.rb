71662b14bac57cae3ec7690d83375b66515d1911
rickyuan <rushyzt@gmail.com>
2011-12-02 01:28:36 +0800

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+  def logo
+    image_tag("rails.png", :alt => "Movie App")
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfmovie::Application.config.secret_token = '5e3ccdc7e7a02df0b37a7ac11382906cff4eb526aa1db9298a0a5c8d5af7a8b925373ba9e80701053d23067fa838b799e7a0a1bcf6214bff53cfe741604e3b90'
